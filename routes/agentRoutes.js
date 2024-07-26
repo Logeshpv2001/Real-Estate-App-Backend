c3876260b96487e09cdfef19ef4d21db373510d7
Logeshpv2001 <logeshkarthi782@gmail.com>
2024-07-26 12:56:06 +0530

    updated

--- routes/agentRoutes.js
+++ routes/agentRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { registerAgent, loginAgent } = require('../controllers/agentController');
 const router = express.Router();
 
-router.post('/api/agents/register', registerAgent);
+router.post('/register', registerAgent);
 router.post('/login', loginAgent);
 
 module.exports = router;
